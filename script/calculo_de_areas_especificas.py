0c982320b91aa466f262887f37f83038b5618809
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2023-08-30 11:41:09 -0300

    Update calculo_de_areas_especificas.py

--- script/calculo_de_areas_especificas.py
+++ script/calculo_de_areas_especificas.py
@@ -1,7 +1,8 @@
+import math
 import numpy as np
 
 from scipy.spatial import cKDTree
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point
 
 # Áreas interiores pelo método das bissetrizes
 
@@ -15,36 +16,50 @@ def areas_e_centroides_de_interiores(poligonos, pontos_de_interseccao, lados_das
         for j in range(len(poligonos[i])-1):
 
             # Área dos polígonos de três vértices
-            if len(poligonos[i]) == 4:
-                coordenadas_do_poligono = (poligonos[i][j], 
-                                           poligonos[i][j+1], 
-                                           pontos_de_interseccao[i][0])
-
+            if len(poligonos[i]) == 4:               
+                areas_dos_poligonos.append(str(Polygon((poligonos[i][j], 
+                                                        poligonos[i][j+1], 
+                                                        pontos_de_interseccao[i][0])).area))
+                
+                coordenada_das_areas_dos_poligonos.append(Polygon((poligonos[i][j], 
+                                                                   poligonos[i][j+1], 
+                                                                   pontos_de_interseccao[i][0])).centroid)
+                
             # Área dos polígonos de quatro vértices
             elif len(poligonos[i]) == 5:
 
-                if (j == lados_das_retas[i][0]):
-                    coordenadas_do_poligono = (poligonos[i][j], 
-                                               poligonos[i][j+1], 
-                                               pontos_de_interseccao[i][0])
+                if (j == lados_das_retas[i][0]):                    
+                    areas_dos_poligonos.append(str(Polygon((poligonos[i][j], 
+                                                            poligonos[i][j+1], 
+                                                            pontos_de_interseccao[i][0])).area))
+                    
+                    coordenada_das_areas_dos_poligonos.append(Polygon((poligonos[i][j], 
+                                                                       poligonos[i][j+1], 
+                                                                       pontos_de_interseccao[i][0])).centroid)
 
                 elif (j == lados_das_retas[i][1]):
-                    coordenadas_do_poligono = (poligonos[i][j], 
-                                               poligonos[i][j+1], 
-                                               pontos_de_interseccao[i][1])
+                    areas_dos_poligonos.append(str(Polygon((poligonos[i][j], 
+                                                            poligonos[i][j+1], 
+                                                            pontos_de_interseccao[i][1])).area))
+                    
+                    coordenada_das_areas_dos_poligonos.append(Polygon((poligonos[i][j], 
+                                                                       poligonos[i][j+1], 
+                                                                       pontos_de_interseccao[i][1])).centroid)
 
                 else:
-                    coordenadas_do_poligono = (poligonos[i][j], 
-                                               poligonos[i][j+1],
-                                               pontos_de_interseccao[i][0],
-                                               pontos_de_interseccao[i][1])
+                    coordenadas, centroide = vertices_de_quadrilateros((poligonos[i][j], 
+                                                                        poligonos[i][j+1], 
+                                                                        pontos_de_interseccao[i][1], 
+                                                                        pontos_de_interseccao[i][0]))
+                    
+                    areas_dos_poligonos.append(str(Polygon(coordenadas[0]).area + Polygon(coordenadas[1]).area))
 
+                    coordenada_das_areas_dos_poligonos.append(Point(centroide))
+                    
             # Área dos polígonos acima de quatro vértices
             else:
-                coordenadas_do_poligono = [(None, None)] * 4
-
-            areas_dos_poligonos.append(str(Polygon(sorted(coordenadas_do_poligono)).area))
-            coordenada_das_areas_dos_poligonos.append(Polygon(sorted(coordenadas_do_poligono)).centroid)
+                areas_dos_poligonos.append(None)
+                coordenada_das_areas_dos_poligonos.append((None,None))
             
     return areas_dos_poligonos, coordenada_das_areas_dos_poligonos
 
@@ -86,7 +101,7 @@ def areas_e_centroides_de_exteriores(poligonos, pontos_da_rua):
                                                       
     return areas_dos_poligonos, coordenada_das_areas_dos_poligonos, linhas_exteriores
 
-# Lados das bissetrizes para poligonos maiores ou iguais a quatro lados
+# Lados das bissetrizes para poligonos iguais ou com mais de quatro vértices
 
 def lados_das_retas_das_bissetrizes(comprimentos):
 
@@ -111,6 +126,7 @@ def lados_das_retas_das_bissetrizes(comprimentos):
         
         # Polígono maior que 4 lados 
         else:
+            
             soma_de_comprimentos, ponteiro = [[],2]
             
             for j in range(len(comprimentos[i])):
@@ -126,3 +142,17 @@ def lados_das_retas_das_bissetrizes(comprimentos):
             lados_das_retas.append((min(soma_de_comprimentos, key=lambda lista: lista[0]))[1])
                                                
     return lados_das_retas
+
+# Ordenando vértices de polígonos com quatro lados para evitar cruzamento de linhas durante calculo de áreas
+
+def vertices_de_quadrilateros(vertices):
+
+    centroide_x = sum(v[0] for v in vertices) / len(vertices)
+    centroide_y = sum(v[1] for v in vertices) / len(vertices)
+
+    vertices_ordenados = sorted(vertices, key=lambda v: math.atan2(v[1] - centroide_y, v[0] - centroide_x))
+
+    coordenadas = ([vertices_ordenados[0], vertices_ordenados[1], vertices_ordenados[2]],
+                   [vertices_ordenados[0], vertices_ordenados[2], vertices_ordenados[3]])
+    
+    return coordenadas, (centroide_x, centroide_y)
