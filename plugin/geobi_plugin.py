f9df51537c86c3991e81e9eb44733d35c177d849
alexandre11aa <alexandrestrela@hotmail.com>
2026-09-26 14:37:13 -0300

    :sparkles: feat: Ajustando compatibilidade para instalação

--- plugin/geobi_plugin.py
+++ plugin/geobi_plugin.py
@@ -4,9 +4,9 @@ import inspect
 import warnings
 import subprocess
 
-from PyQt5.QtCore import * 
-from PyQt5.QtGui import * 
-from PyQt5.QtWidgets import *
+from qgis.PyQt.QtCore import *
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import *
 
 from qgis.core import QgsProcessingAlgorithm, QgsApplication
 
